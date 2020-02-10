72cbcbd32bb7c58e980b6827decc9d7d074dfae1
palgang <palgangmori@gmail.com>
2020-02-10 16:57:45 +0100

    logwarn for bug fixing

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -121,6 +121,9 @@ class WaypointUpdater(object):
             p.pose = wp.pose
 
             stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0)
+            rospy.logwarn("i: {0}".format(i))
+            rospy.logwarn("stop_idx: {0}".format(stop_idx))
+            rospy.logwarn("waypoints: {0}".format(waypoints))
             dist = self.distance(waypoints, i, stop_idx)
             vel = math.sqrt(2 * MAX_DECEL * dist)
 
