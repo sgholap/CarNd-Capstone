2e001096b45d66a0d00ccd2738feb34395eba9f7
Zhenghao Xu <zhenghao.xu@zongmutech.com>
2020-02-18 22:05:42 +0800

    add trained model for simulator and update tl detector and classifier, not tested

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,17 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+import tensorflow as tf
+import rospy
+import numpy as np
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        self.graph = tf.get_default_graph()
+        self.model = None
+
+    def set_model(self, model):
+        self.model = model
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +24,24 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        resized_img = cv2.resize(img / 255., (60, 80))
+        with self.graph.as_default():
+            predictions = self.model.predict(resized_img.reshape((1, 80, 60, 3)))
+            result = predictions[0].tolist().index(np.max(predictions[0]))
+            traffic_light = TrafficLight()
+            if result == 0:
+                # no light
+                traffic_light.state = 4
+            if result == 1:
+                # red
+                traffic_light.state = 0
+            if result == 2:
+                # yellow
+                traffic_light.state = 1
+            if result == 3: 
+                # green
+                traffic_light.state = 2
+#             traffic_light.state = result
+            rospy.loginfo("model output: %s", traffic_light.state)
+        return traffic_light.state
